d1f1118c09ee143abdf52bf9c79d14e1fb4cb85b
Manida Vilay <vilaymanida@gmail.com>
2025-01-10 16:18:23 +0100

    add react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Signup from "./pages/Auth/Signup/Signup";
+import Login from "./pages/Auth/Login/Login";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
