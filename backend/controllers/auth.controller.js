d8a449da318cf0f1d22ea9ca15e11e6d21c52313
Manida Vilay <vilaymanida@gmail.com>
2025-01-13 22:04:30 +0100

    add get user route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -11,18 +11,19 @@ const registerUser = async (req, res) => {
 
     const existingUser = await User.findOne({ email });
     if (existingUser)
-      return res.status(400).json({ message: "User already exists !" });
+      return res.status(409).json({ message: "User already exists !" });
 
     const hashedPassword = await bcrypt.hash(password, 12);
     const newUser = new User({ email, fullName, password: hashedPassword });
     await newUser.save();
+
     const accessToken = jwt.sign(
-      { user: newUser._id },
+      { userId: newUser._id },
       process.env.ACCESS_TOKEN,
       { expiresIn: "1h" }
     );
     return res.status(201).json({
-      user: { fullName: newUser.fullName, email: newUser.email },
+      user: { fullName: newUser.fullName, email: usnewUserer.email },
       accessToken,
       message: "Account successfully created !",
     });
@@ -40,20 +41,28 @@ const loginUser = async (req, res) => {
         .json({ message: "Email and password are required" });
     }
 
-    const user = await User.findOne({ email });
-    if (!user) return res.status(404).json({ message: "User not found" });
+    const existingUser = await User.findOne({ email });
+    if (!existingUser)
+      return res.status(404).json({ message: "User not found" });
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
     if (!isPasswordValid) {
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    const accessToken = jwt.sign({ user: user._id }, process.env.ACCESS_TOKEN, {
-      expiresIn: "1h",
-    });
+    const accessToken = jwt.sign(
+      { userId: existingUser._id },
+      process.env.ACCESS_TOKEN,
+      {
+        expiresIn: "1h",
+      }
+    );
     return res.status(200).json({
       message: "Login successful !",
-      user: { fullName: user.fullName, email: user.email },
+      user: { fullName: existingUser.fullName, email: existingUser.email },
       accessToken,
     });
   } catch (error) {
@@ -61,4 +70,20 @@ const loginUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser, loginUser };
+const getUser = async (req, res) => {
+  const { userId } = req.user;
+  try {
+    const existingUser = await User.findOne({ _id: userId });
+    if (!existingUser)
+      return res.status(404).json({ message: "User not found" });
+
+    return res.status(200).json({
+      user: existingUser,
+      message: "User successfully found !",
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error. Please try again later" });
+  }
+};
+
+module.exports = { registerUser, loginUser, getUser };
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/auth.controller");
+const {
+  registerUser,
+  loginUser,
+  getUser,
+} = require("../controllers/auth.controller");
+const { authenticateToken } = require("../middlewares/authMiddleware");
 
 // Register user route
 router.post("/create-account", registerUser);
 
 // Login user route
 router.post("/login", loginUser);
+
+// Get user route
+router.get("/get-user", authenticateToken, getUser);
 
 module.exports = router;
