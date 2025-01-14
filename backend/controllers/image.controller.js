523b9d4c85f10e151afce5d9b29ce88b9002cf25
Manida Vilay <vilaymanida@gmail.com>
2025-01-14 16:52:12 +0100

    add delete image api

--- backend/controllers/image.controller.js
+++ backend/controllers/image.controller.js
@@ -16,4 +16,27 @@ const handleImageUpload = async (req, res) => {
   }
 };
 
-module.exports = { handleImageUpload };
+// Delete Image API
+const deleteImage = async (req, res) => {
+  const { imageUrl } = req.query;
+
+  if (!imageUrl) {
+    return res.status(400).json({ message: "imageUrl parameter is required" });
+  }
+
+  try {
+    const filename = path.basename(imageUrl);
+    const filePath = path.join(__dirname, "..", "uploads", filename);
+
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+      res.status(200).json({ message: "Image successfully deleted !" });
+    } else {
+      res.status(404).json({ message: "Image not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server error. Please try again" });
+  }
+};
+
+module.exports = { handleImageUpload, deleteImage };
--- backend/routes/image.route.js
+++ backend/routes/image.route.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const upload = require("../multer");
 const router = express.Router();
-const { handleImageUpload } = require("../controllers/image.controller");
+const {
+  handleImageUpload,
+  deleteImage,
+} = require("../controllers/image.controller");
 
 // Handle Image Upload Route
 router.post("/upload", upload.single("image"), handleImageUpload);
+
+// Delete Image Route
+router.delete("/delete", deleteImage);
 
 module.exports = router;
